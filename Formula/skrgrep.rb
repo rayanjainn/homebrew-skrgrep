9bc8120334b9bdf30e61def2779c5a819504f535
Rayan Jain <144351257+rayanjainn@users.noreply.github.com>
2026-03-12 13:03:09 +0530

    Update homepage and URLs for skrgrep

--- Formula/skrgrep.rb
+++ Formula/skrgrep.rb
@@ -1,14 +1,14 @@
 class Skrgrep < Formula
   desc "Fast multi-threaded grep in Rust"
-  homepage "https://github.com/rayanjainn/minigrep"
+  homepage "https://github.com/rayanjainn/skrgrep"
   version "0.1.1"
 
   # We define both URLs here
   if Hardware::CPU.arm?
-    url "https://github.com/rayanjainn/minigrep/releases/download/v0.1.1/skrgrep-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/rayanjainn/skrgrep/releases/download/v0.1.1/skrgrep-aarch64-apple-darwin.tar.gz"
     sha256 "HASH_OF_THE_ARM_FILE"
   else
-    url "https://github.com/rayanjainn/minigrep/releases/download/v0.1.1/skrgrep-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/rayanjainn/skrgrep/releases/download/v0.1.1/skrgrep-x86_64-apple-darwin.tar.gz"
     sha256 "HASH_OF_THE_INTEL_FILE"
   end
 
