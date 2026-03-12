31d618a5a63a9a8e09e0e4a2fed423ba0ee84441
Rayan Jain <144351257+rayanjainn@users.noreply.github.com>
2026-03-12 13:09:02 +0530

    Update SHA256 checksums for skrgrep binaries

--- Formula/skrgrep.rb
+++ Formula/skrgrep.rb
@@ -6,10 +6,10 @@ class Skrgrep < Formula
   # We define both URLs here
   if Hardware::CPU.arm?
     url "https://github.com/rayanjainn/skrgrep/releases/download/v0.1.1/skrgrep-aarch64-apple-darwin.tar.gz"
-    sha256 "HASH_OF_THE_ARM_FILE"
+    sha256 "c4c360a172c277e7865d23e919a157ae750f177a75f873a5044814a4ac7feae8"
   else
     url "https://github.com/rayanjainn/skrgrep/releases/download/v0.1.1/skrgrep-x86_64-apple-darwin.tar.gz"
-    sha256 "HASH_OF_THE_INTEL_FILE"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
   def install
